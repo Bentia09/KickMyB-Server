e8915fa6f73834c8dcf5aafb32fac3530ec8579a
Bentia09 <130761295+Bentia09@users.noreply.github.com>
2025-04-23 17:18:49 -0400

    Update ServiceAccount.java

--- src/main/java/org/kickmyb/server/account/ServiceAccount.java
+++ src/main/java/org/kickmyb/server/account/ServiceAccount.java
@@ -7,29 +7,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public interface ServiceAccount extends UserDetailsService {
 
   public class UsernameTooShort extends Exception {
-    public UsernameTooShort(String lang) {
-        super(lang != null && lang.startsWith("en")
-            ? "Username is too short."
-            : "Le nom d'utilisateur est trop court.");
-    }
+  
 }
 
   public class UsernameAlreadyTaken extends Exception {
-    public UsernameAlreadyTaken(String lang) {
-        super(lang != null && lang.startsWith("en")
-            ? "This username is already taken."
-            : "Ce nom d'utilisateur est déjà pris.");
-    }
+    
 }
 
    public class PasswordTooShort extends Exception {
-    public PasswordTooShort(String lang) {
-        super(lang != null && lang.startsWith("en")
-            ? "Password is too short."
-            : "Le mot de passe est trop court.");
-    }
+   
 }
 
- void signup(SignupRequest req, String lang) throws BadCredentialsException, UsernameTooShort, PasswordTooShort, UsernameAlreadyTaken;
+ void signup(SignupRequest req) throws BadCredentialsException, UsernameTooShort, PasswordTooShort, UsernameAlreadyTaken;
 
 }
