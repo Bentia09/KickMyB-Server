d13dd849c6120592cb18ec3d865246be2f3fd952
Bentia09 <130761295+Bentia09@users.noreply.github.com>
2025-04-23 15:42:48 -0400

    Update ServiceAccount.java

--- src/main/java/org/kickmyb/server/account/ServiceAccount.java
+++ src/main/java/org/kickmyb/server/account/ServiceAccount.java
@@ -6,10 +6,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 // extends UserDetailsService which is one of the Spring Security entry points
 public interface ServiceAccount extends UserDetailsService {
 
-    class UsernameTooShort extends Exception {}
-    class UsernameAlreadyTaken extends Exception {}
-    class PasswordTooShort extends Exception {}
-
+    class UsernameTooShort extends Exception {
+ public UsernameTooShort() {
+            super("Le nom d'utilisateur est trop court.");
+        }
+        
+    }
+    class UsernameAlreadyTaken extends Exception {
+          public UsernameAlreadyTaken() {
+            super("Ce nom d'utilisateur est déjà pris.");
+        }
+    }
+    class PasswordTooShort extends Exception {
+        public PasswordTooShort() {
+            super("Le mot de passe est trop court.");
+        }
+    }
     void signup(SignupRequest req) throws BadCredentialsException, UsernameTooShort, PasswordTooShort, UsernameAlreadyTaken;
 
 }
