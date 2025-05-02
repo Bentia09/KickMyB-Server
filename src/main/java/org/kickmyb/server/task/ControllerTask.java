66329493c88857606d2ea6aa4d4f1ed9ce6d24d1
Bentia09 <130761295+Bentia09@users.noreply.github.com>
2025-05-02 17:22:41 -0400

    Update ControllerTask.java

--- src/main/java/org/kickmyb/server/task/ControllerTask.java
+++ src/main/java/org/kickmyb/server/task/ControllerTask.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
